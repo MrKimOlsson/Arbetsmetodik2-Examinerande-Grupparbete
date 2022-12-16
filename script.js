452923dd183bcc16d65cc265a60fc21748fd99df
Evelina Nyman <ina.nyman98@gmail.com>
2022-12-16 13:26:34 +0100

    färdig med footer, redo för testning

--- script.js
+++ script.js
@@ -34,10 +34,12 @@ hamburger.addEventListener("click", toggleMenu);
     x.classList.toggle("change");
   }
 
+  
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
     this.classList.toggle("active");
+    this.classList.toggle("rotatePlus");
 
     /* Toggle between hiding and showing the active panel */
     let panel = this.nextElementSibling;
