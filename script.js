1aa224aee7b94a9e3c2a7d8520fba4000f301496
Kim Olsson <mr.kim.olsson@gmail.com>
2022-12-21 03:15:44 +0100

    slider returns to first slide on last sliden & indicator dot return to first dot on restart

--- script.js
+++ script.js
@@ -34,54 +34,96 @@ hamburger.addEventListener("click", toggleMenu);
     x.classList.toggle("change");
   }
 
+// _______________________________________________________________________________________
 
+
+// __________________________________IMAGE CAROUSEL SLIDER_________________________________
   // Image carousel slider
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel__button--right');
 const prevButton = document.querySelector('.carousel__button--left');
 const dotsNav = document.querySelector('.carousel__nav');
+const firstDot = document.querySelector('.firstDot');
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-// console.log(slideWidth);
+
+let slideNumber = [];
+let imgNr = 0;
+
+const currentSlide = track.querySelector('.current-slide');
+// let nextSlide = currentSlide;
+let prevSlide = currentSlide.previousElementSibling;
+let lastSlide = slides[9];  
+let firstSlide = slides[0];
+let restart = false;
+
+const setSlidePosition = () => {
+
+  slides[0].style.left = slideWidth * 0 + 'px';
+  slides[1].style.left = slideWidth * 1 + 'px';
+  slides[2].style.left = slideWidth * 2 + 'px';
+  slides[3].style.left = slideWidth * 3 + 'px';
+  slides[4].style.left = slideWidth * 4 + 'px';
+  slides[5].style.left = slideWidth * 5 + 'px';
+  slides[6].style.left = slideWidth * 6 + 'px';
+  slides[7].style.left = slideWidth * 7 + 'px';
+  slides[8].style.left = slideWidth * 8 + 'px';
 
 
-// FIX THIS PART!
-// ________________________________________________________________________
-
-// arrange images
-
-// slides[0].style.left = 0;
-// slides[1].style.left = '600px';
-
-// ________________________________________________________________________
-
-// slides[0].style.left = slideWidth * 0 + 'px';
-// slides[1].style.left = slideWidth * 1 + 'px';
-// slides[2].style.left = slideWidth * 2 + 'px';
-
-
-// ________________________________________________________________________
-
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + 'px';
 }
-
+console.log(slideNumber);
+//  for each slide image = set slide position§
 slides.forEach(setSlidePosition);
 
 // ________________________________________________________________________
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
-  currentSlide.classList.remove('current-slide');
-  targetSlide.classList.add('current-slide');
+  nextSlide = currentSlide.nextElementSibling;
+
+  if(restart) {
+    // track.style.transform = 'translateX(0px)';
+    firstSlide.classList.add('current-slide');
+    console.log("restart");
+    restart = false;
+  }
+
+  else if(nextSlide == lastSlide){
+    console.log("next slide = last");
+    currentSlide.classList.remove('current-slide');
+    firstSlide.classList.add('current-slide');
+    nextSlide = currentSlide.nextElementSibling;
+    restart = true;
+    track.style.transform = 'translateX(0px)';
+  }
+
+  else {
+    console.log("next slide");
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+
+    nextSlide = currentSlide.nextElementSibling;
+    prevSlide = currentSlide.previousElementSibling;
+
+    console.log(nextSlide);
+    console.log(lastSlide);
+  }
 }
 
 const updateDots = (currentDot, targetDot) => {
-  currentDot.classList.remove('current-slide');
-  targetDot.classList.add('current-slide');
+  
+  if(restart){
+    currentDot.classList.remove('current-slide');
+    firstDot.classList.add('current-slide');
+    restart = false;
+  }
+  else {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+  }
 
 }
 
@@ -89,10 +131,11 @@ const updateDots = (currentDot, targetDot) => {
 // And update indicator dots
 prevButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide');
-  const prevSlide = currentSlide.previousElementSibling;
+
+  let prevSlide = currentSlide.previousElementSibling;
   const currentDot = dotsNav.querySelector('.current-slide');
   const prevDot = currentDot.previousElementSibling;
-
+  
   moveToSlide(track, currentSlide, prevSlide);
   updateDots(currentDot, prevDot);
 })
@@ -101,16 +144,29 @@ prevButton.addEventListener('click', e => {
 // And update indicator dots
 nextButton.addEventListener('click', e => {
   const currentSlide = track.querySelector('.current-slide');
-  const nextSlide = currentSlide.nextElementSibling;
-  const currentDot = dotsNav.querySelector('.current-slide');
-  const nextDot = currentDot.nextElementSibling;
+  const carouselTrack = track.querySelector('.carousel__track');
+  // const firstDot = dotsNav.querySelector('.first-dot');
 
-  moveToSlide(track, currentSlide, nextSlide);
-  updateDots(currentDot, nextDot);
+  if(restart){
+    nextSlide.classList.add('firstSlide');
+    currentSlide.classList.remove('.current-slide');
+    // firstDot.classList.add('.current-slide');
+    track.style.transform = 'translateX(+' + nextSlide.style.left + ')';
+    // restart = false;
+    moveToSlide(track, currentSlide, nextSlide);
+    // updateDots(currentDot, firstDot);
+  }
+  else {
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+  }
+  
 })
 
 // When click on nav indivators, move to that slide
-
 dotsNav.addEventListener('click', e => {
   // what indicator got clicked?
   const targetDot = e.target.closest('button');
@@ -123,15 +179,12 @@ dotsNav.addEventListener('click', e => {
   const targetSlide = slides[targetIndex];
 
   moveToSlide(track, currentSlide, targetSlide);
-
-  
   updateDots(currentDot, targetDot);
   hideShowArrows(slides, prevButton, nextButton, targetIndex);
-
-
-
 })
   
+// _____________________________________Footer_________________________________________
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
